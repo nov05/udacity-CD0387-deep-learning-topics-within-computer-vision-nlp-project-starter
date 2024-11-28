4e6f9aa80c1c3f652b930c245e704b2781330841
nov05 <215590+Nov05@users.noreply.github.com>
2024-11-28 06:29:56 -0600

    course 4 notebooks, collected, not done

---
+++ cd0387_deploy_deeplearning_models_on_sagemaker/script mode/script_mode.py
@@ -0,0 +1,8 @@
+from sagemaker.pytorch import PyTorch
+from sagemaker import get_execution_role
+
+hyperparameters = # TODO: Include the hyperparameters your script will need over here.
+
+estimator = # TODO: Create your estimator here. You can use Pytorch or any other framework.
+
+#TODO: Start Training
---
+++ cd0387_deploy_deeplearning_models_on_sagemaker/script mode/scripts/pytorch_cifar.py
@@ -0,0 +1,76 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+class Net(nn.Module):
+    def __init__(self):
+        #TODO: Complete this function
+        
+    def forward(self, x):
+        #TODO: Complete the forward function
+
+def train(model, train_loader, optimizer, epoch):
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
+
+
+def test(model, test_loader):
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
+    # TODO: Add your arguments here
+    args = parser.parse_args()
+
+    transform = # TODO: Create your transform
+    
+    # TODO: Add the CIFAR10 dataset and create your data loaders
+    dataset = 
+    train_loader =
+    test_loader = 
+
+    model = Net()
+
+    optimizer = # TODO: Add your optimizer
+
+    for epoch in range(1, args.epochs + 1):
+        train(model, train_loader, optimizer, epoch)
+        test(model, test_loader)
+
+
+if __name__ == "__main__":
+    main()
